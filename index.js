ce7dc647b9fb336a30ece21a96a2d8e304d3c08f
andrias97 <78638698+andrias97@users.noreply.github.com>
2021-02-06 19:24:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ const { ind } = require('./language')
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
             + 'FN:Affis Admin\n' 
-            + 'ORG: Pengembang XBot;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
+            + 'ORG: Pengembang XBot;\n'
             + 'END:VCARD' 
 prefix = '#'
 blocked = []   
